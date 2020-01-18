68ce095b1d4b82b69d9247562ed8303e0ad029a7
Catherine Hall <catherine1992123@hotmail.com>
2020-01-18 16:08:31 +0000

    Updated text

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="app">
       <nav>
-      <h1>Game of Thrones</h1>
+      <h1>The Houses of Westeros</h1>
+      <p>For many Games of Thrones fans, it's the additional details that George R.R Martin has added, such as the well-documented family trees and comprehesive maps, that help to important part in bringing the world that his characters inhabit to life. The kingdom of Westeros, where much of the story takes place, is made up of many ruling families.  To find out more about these houses, please click on the option below.</p>
     </nav>
     <house-list :houses="houses" />
     <house-detail />
